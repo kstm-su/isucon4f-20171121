84b8c8ed80d9c347de83a798a626a713d8b1c555
bgpat <bgpat@users.noreply.github.com>
2017-11-21 11:54:52 +0000

    Flush redis on initializing

--- home/isucon/webapp/go/init.go
+++ home/isucon/webapp/go/init.go
@@ -7,15 +7,7 @@ import (
 )
 
 func routePostInitialize(c *gin.Context) {
-	keys, _ := rd.Keys("isu4:*").Result()
-	for i := range keys {
-		key := keys[i]
-		rd.Del(key)
-	}
-	path := getDir("log")
-	os.RemoveAll(path)
-
+	rd.FlushDb()
 	os.RemoveAll("/home/isucon/webapp/public/slots/")
-
 	c.String(200, "OK")
 }
