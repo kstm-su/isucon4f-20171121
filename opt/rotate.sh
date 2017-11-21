4b5f1215dfba67d34f88e45845806a7e665d2fd1
fono09 <fono09@users.noreply.github.com>
2017-11-21 06:27:35 +0000

    Fix rotate.sh

--- opt/rotate.sh
+++ opt/rotate.sh
@@ -1,4 +1,4 @@
 cp /tmp/kataribe.log /tmp/kataribe.log.1
-/opt/kataribe -conf /opt/kataribe.toml < /var/log/nginx/access_log > /tmp/kataribe.log
-cp /var/log/nginx/access_log /var/log/nginx/access_log.1
->/var/log/nginx/access_log
+/opt/kataribe -conf /opt/kataribe.toml < /var/log/nginx/access.log > /tmp/kataribe.log
+cp /var/log/nginx/access.log /var/log/nginx/access.log.1
+>/var/log/nginx/access.log
